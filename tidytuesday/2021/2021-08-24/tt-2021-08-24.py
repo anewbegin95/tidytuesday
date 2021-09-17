64357d23bca17996ad45e8e03fd716de423810e4
Alex Newbegin <47902821+anewbegin95@users.noreply.github.com>
2021-09-17 13:47:54 -0400

    Add files via upload
    Updated script with framework for building family tree

--- tidytuesday/2021/2021-08-24/tt-2021-08-24.py
+++ tidytuesday/2021/2021-08-24/tt-2021-08-24.py
@@ -7,6 +7,7 @@ Created on Thu Aug 26 19:52:32 2021
 """
 # %% import pandas module
 import pandas as pd
+from graphviz import Digraph
 
 # %% import lemur data
 lemur_data_url = r'https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-08-24/lemur_data.csv'
@@ -26,11 +27,13 @@ for col in taxonomy:
     print(col)
 
 # %% join datasets on taxonomy
-lemur_data = lemur_data.join(tax_url)
+lemur_data = lemur_data.join(taxonomy)
 
 print(lemur_data.head(5))
 for col in lemur_data:
     print(col)
     
-# %%
-print(taxonomy.index)
+# %% print lemur's name, and the name of its dam and sire
+lemur_name = lemur_data[['dlc_id','name', 'dam_id', 'dam_name', 'sire_id', 'sire_name']]
+lemur_name = lemur_name.drop_duplicates()
+print(lemur_name)
